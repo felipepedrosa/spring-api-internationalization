ba4f920370b095b11068765d32e18fbded292927
felipepedrosa <felipeepedrosa@gmail.com>
2021-12-13 09:59:00 -0300

    Spring Boot auto-generated files

---
+++ src/main/java/io/github/felipepedrosa/springapiinternationalization/SpringApiInternationalizationApplication.java
@@ -0,0 +1,13 @@
+package io.github.felipepedrosa.springapiinternationalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiInternationalizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiInternationalizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/felipepedrosa/springapiinternationalization/SpringApiInternationalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felipepedrosa.springapiinternationalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiInternationalizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
